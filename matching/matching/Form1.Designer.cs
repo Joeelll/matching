5b0616c4bbde3a070905157c37048382b6afc069
Joel <31409053+Joeelll@users.noreply.github.com>
2018-01-10 09:17:43 +0200

    53tr3ete

--- matching/matching/Form1.Designer.cs
+++ matching/matching/Form1.Designer.cs
@@ -28,43 +28,293 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CardsHolder = new System.Windows.Forms.Panel();
+            this.DupCard8 = new System.Windows.Forms.PictureBox();
+            this.Card8 = new System.Windows.Forms.PictureBox();
+            this.DupCard7 = new System.Windows.Forms.PictureBox();
+            this.Card7 = new System.Windows.Forms.PictureBox();
+            this.DupCard6 = new System.Windows.Forms.PictureBox();
+            this.Card6 = new System.Windows.Forms.PictureBox();
+            this.DupCard5 = new System.Windows.Forms.PictureBox();
+            this.Card5 = new System.Windows.Forms.PictureBox();
+            this.DupCard4 = new System.Windows.Forms.PictureBox();
+            this.Card4 = new System.Windows.Forms.PictureBox();
+            this.DupCard3 = new System.Windows.Forms.PictureBox();
+            this.Card3 = new System.Windows.Forms.PictureBox();
+            this.DupCard2 = new System.Windows.Forms.PictureBox();
+            this.Card2 = new System.Windows.Forms.PictureBox();
+            this.DupCard1 = new System.Windows.Forms.PictureBox();
+            this.Card1 = new System.Windows.Forms.PictureBox();
+            this.CardsHolder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // label1
             // 
-            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(645, 420);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(537, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score: ";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Score.Location = new System.Drawing.Point(623, 15);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(24, 25);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CardsHolder
+            // 
+            this.CardsHolder.Controls.Add(this.DupCard8);
+            this.CardsHolder.Controls.Add(this.Card8);
+            this.CardsHolder.Controls.Add(this.DupCard7);
+            this.CardsHolder.Controls.Add(this.Card7);
+            this.CardsHolder.Controls.Add(this.DupCard6);
+            this.CardsHolder.Controls.Add(this.Card6);
+            this.CardsHolder.Controls.Add(this.DupCard5);
+            this.CardsHolder.Controls.Add(this.Card5);
+            this.CardsHolder.Controls.Add(this.DupCard4);
+            this.CardsHolder.Controls.Add(this.Card4);
+            this.CardsHolder.Controls.Add(this.DupCard3);
+            this.CardsHolder.Controls.Add(this.Card3);
+            this.CardsHolder.Controls.Add(this.DupCard2);
+            this.CardsHolder.Controls.Add(this.Card2);
+            this.CardsHolder.Controls.Add(this.DupCard1);
+            this.CardsHolder.Controls.Add(this.Card1);
+            this.CardsHolder.Location = new System.Drawing.Point(12, 15);
+            this.CardsHolder.Name = "CardsHolder";
+            this.CardsHolder.Size = new System.Drawing.Size(519, 394);
+            this.CardsHolder.TabIndex = 3;
+            // 
+            // DupCard8
+            // 
+            this.DupCard8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard8.Location = new System.Drawing.Point(390, 291);
+            this.DupCard8.Name = "DupCard8";
+            this.DupCard8.Size = new System.Drawing.Size(123, 90);
+            this.DupCard8.TabIndex = 21;
+            this.DupCard8.TabStop = false;
+            // 
+            // Card8
+            // 
+            this.Card8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card8.Location = new System.Drawing.Point(261, 291);
+            this.Card8.Name = "Card8";
+            this.Card8.Size = new System.Drawing.Size(123, 90);
+            this.Card8.TabIndex = 20;
+            this.Card8.TabStop = false;
+            // 
+            // DupCard7
+            // 
+            this.DupCard7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard7.Location = new System.Drawing.Point(132, 291);
+            this.DupCard7.Name = "DupCard7";
+            this.DupCard7.Size = new System.Drawing.Size(123, 90);
+            this.DupCard7.TabIndex = 19;
+            this.DupCard7.TabStop = false;
+            // 
+            // Card7
+            // 
+            this.Card7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card7.Location = new System.Drawing.Point(3, 291);
+            this.Card7.Name = "Card7";
+            this.Card7.Size = new System.Drawing.Size(123, 90);
+            this.Card7.TabIndex = 18;
+            this.Card7.TabStop = false;
+            // 
+            // DupCard6
+            // 
+            this.DupCard6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard6.Location = new System.Drawing.Point(390, 195);
+            this.DupCard6.Name = "DupCard6";
+            this.DupCard6.Size = new System.Drawing.Size(123, 90);
+            this.DupCard6.TabIndex = 15;
+            this.DupCard6.TabStop = false;
+            // 
+            // Card6
+            // 
+            this.Card6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card6.Location = new System.Drawing.Point(261, 195);
+            this.Card6.Name = "Card6";
+            this.Card6.Size = new System.Drawing.Size(123, 90);
+            this.Card6.TabIndex = 14;
+            this.Card6.TabStop = false;
+            // 
+            // DupCard5
+            // 
+            this.DupCard5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard5.Location = new System.Drawing.Point(132, 195);
+            this.DupCard5.Name = "DupCard5";
+            this.DupCard5.Size = new System.Drawing.Size(123, 90);
+            this.DupCard5.TabIndex = 13;
+            this.DupCard5.TabStop = false;
+            // 
+            // Card5
+            // 
+            this.Card5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card5.Location = new System.Drawing.Point(3, 195);
+            this.Card5.Name = "Card5";
+            this.Card5.Size = new System.Drawing.Size(123, 90);
+            this.Card5.TabIndex = 12;
+            this.Card5.TabStop = false;
+            // 
+            // DupCard4
+            // 
+            this.DupCard4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard4.Location = new System.Drawing.Point(390, 99);
+            this.DupCard4.Name = "DupCard4";
+            this.DupCard4.Size = new System.Drawing.Size(123, 90);
+            this.DupCard4.TabIndex = 9;
+            this.DupCard4.TabStop = false;
+            // 
+            // Card4
+            // 
+            this.Card4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card4.Location = new System.Drawing.Point(261, 99);
+            this.Card4.Name = "Card4";
+            this.Card4.Size = new System.Drawing.Size(123, 90);
+            this.Card4.TabIndex = 8;
+            this.Card4.TabStop = false;
+            // 
+            // DupCard3
+            // 
+            this.DupCard3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard3.Location = new System.Drawing.Point(132, 99);
+            this.DupCard3.Name = "DupCard3";
+            this.DupCard3.Size = new System.Drawing.Size(123, 90);
+            this.DupCard3.TabIndex = 7;
+            this.DupCard3.TabStop = false;
+            // 
+            // Card3
+            // 
+            this.Card3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card3.Location = new System.Drawing.Point(3, 99);
+            this.Card3.Name = "Card3";
+            this.Card3.Size = new System.Drawing.Size(123, 90);
+            this.Card3.TabIndex = 6;
+            this.Card3.TabStop = false;
+            // 
+            // DupCard2
+            // 
+            this.DupCard2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard2.Location = new System.Drawing.Point(390, 3);
+            this.DupCard2.Name = "DupCard2";
+            this.DupCard2.Size = new System.Drawing.Size(123, 90);
+            this.DupCard2.TabIndex = 3;
+            this.DupCard2.TabStop = false;
+            // 
+            // Card2
+            // 
+            this.Card2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card2.Location = new System.Drawing.Point(261, 3);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(123, 90);
+            this.Card2.TabIndex = 2;
+            this.Card2.TabStop = false;
+            // 
+            // DupCard1
+            // 
+            this.DupCard1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.DupCard1.Location = new System.Drawing.Point(132, 3);
+            this.DupCard1.Name = "DupCard1";
+            this.DupCard1.Size = new System.Drawing.Size(123, 90);
+            this.DupCard1.TabIndex = 1;
+            this.DupCard1.TabStop = false;
+            // 
+            // Card1
+            // 
+            this.Card1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Card1.Location = new System.Drawing.Point(3, 3);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(123, 90);
+            this.Card1.TabIndex = 0;
+            this.Card1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 468);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(653, 421);
+            this.Controls.Add(this.CardsHolder);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.CardsHolder.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DupCard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel CardsHolder;
+        private System.Windows.Forms.PictureBox DupCard8;
+        private System.Windows.Forms.PictureBox Card8;
+        private System.Windows.Forms.PictureBox DupCard7;
+        private System.Windows.Forms.PictureBox Card7;
+        private System.Windows.Forms.PictureBox DupCard6;
+        private System.Windows.Forms.PictureBox Card6;
+        private System.Windows.Forms.PictureBox DupCard5;
+        private System.Windows.Forms.PictureBox Card5;
+        private System.Windows.Forms.PictureBox DupCard4;
+        private System.Windows.Forms.PictureBox Card4;
+        private System.Windows.Forms.PictureBox DupCard3;
+        private System.Windows.Forms.PictureBox Card3;
+        private System.Windows.Forms.PictureBox DupCard2;
+        private System.Windows.Forms.PictureBox Card2;
+        private System.Windows.Forms.PictureBox DupCard1;
+        private System.Windows.Forms.PictureBox Card1;
     }
 }
 
--- matching/matching/Form1.cs
+++ matching/matching/Form1.cs
@@ -12,9 +12,43 @@ namespace matching
 {
     public partial class Form1 : Form
     {
+        int score = 0;
+        Random Location = new Random();
+        List<int> X = new List<int>();
+        List<int> Y = new List<int>();
+        bool again = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Card1.Image = Properties.Resources.Card1;
+            DupCard1.Image = Properties.Resources.Card1;
+            Card2.Image = Properties.Resources.Card2;
+            DupCard2.Image = Properties.Resources.Card2;
+            Card3.Image = Properties.Resources.Card3;
+            DupCard3.Image = Properties.Resources.Card3;
+            Card4.Image = Properties.Resources.Card4;
+            DupCard4.Image = Properties.Resources.Card4;
+            Card5.Image = Properties.Resources.Card5;
+            DupCard5.Image = Properties.Resources.Card5;
+            Card6.Image = Properties.Resources.Card6;
+            DupCard6.Image = Properties.Resources.Card6;
+            Card7.Image = Properties.Resources.Card7;
+            DupCard7.Image = Properties.Resources.Card7;
+            Card8.Image = Properties.Resources.Card8;
+            DupCard8.Image = Properties.Resources.Card8;          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox picture in CardsHolder.Controls)
+            {
+                picture.Image = null;
+            }
         }
     }
 }
